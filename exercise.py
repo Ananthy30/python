b0519e74b28cbca0359f2ec14ffa4af0eb6ec1b0
Ananthy30 <anandhi.arun2@gmail.com>
2026-02-18 13:35:38 +0530

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -10,11 +10,6 @@ x = 50
 class Item(BaseModel):
     mile : int
 
-@router.get('/one')
-
-def oneDay():
-    return x
-
 @router.get('/permonth')
 
 def perMonth():
@@ -39,4 +34,47 @@ def find():
      return 'found lion'
     except:
      return 'no animals found'
-    
+
+@router.get('/list')
+
+def listEx(limit:int):
+    listArray  = list(range(1,limit))
+    return listArray
+
+@router.get('/dictionaries')
+
+def getData():
+    persons = {"Jeff": "Is afraid of clowns.", "David":" Plays the piano."}
+    return persons
+
+@router.get('/keys')
+
+def getData():
+    persons = {"Jeff": "Is afraid of clowns.", "David":" Plays the piano."}
+    persons['Jeff'] = "He is a teacher"
+    persons['henry'] = "New to town"    
+    return persons
+
+@router.get('/tuples')
+
+# list of tuples
+def getData():
+    airports = [('bglr','123'),('chennai','456')]
+    data = []
+    for airport in airports:
+        name = airport[0]
+        code = airport[1]
+        data.append({"name":name,"code":code})
+        
+    return data
+
+# Functions Assignment
+# - Create a function that takes in 3 parameters(firstname, lastname, age) and
+# returns a dictionary based on those values
+
+# http://localhost:8000/test?user=ananthy&id=1&email=sdd
+@router.get('/test')
+
+def getDict(user,id,email):
+    return {"user":user,"id":id,"email":email}
+
